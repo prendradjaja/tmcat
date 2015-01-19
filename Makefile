c53accbd7f9e2c9aa15bdd09054fb03f1be62337
Pandu <prendradjaja@gmail.com>
2015-01-18 18:39:49 -0800

    Rewrite tests in Mocha; add tests to makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,13 @@
 tmcat.js : tmcat.ts
 	tsc tmcat.ts --module commonjs
 
-.PHONY : clean
+.PHONY : clean tests runtests
 clean :
 	rm tmcat.js
+
+tests :
+	tsc simple-tests/test.ts --module commonjs
+
+runtests :
+	cd simple-tests;\
+	../node_modules/mocha/bin/mocha test.js
---
+++ simple-tests/test.ts
@@ -0,0 +1,33 @@
+/// <reference path="../external/mocha.d.ts" />
+/// <reference path="../external/chai.d.ts" />
+/// <reference path="../external/shelljs.d.ts" />
+
+import sh = require('shelljs');
+import chai = require('chai');
+var assert = chai.assert;
+
+function tmcat(args) {
+    return sh.exec('../tmcat ' + args, {silent:true}).output;
+}
+
+describe('tmcat', function () {
+    function clean() {
+        sh.rm('-f', 'MyModule.ts');
+    }
+
+    beforeEach(clean);
+    after(clean);
+
+    it('should generate a .ts file correctly', function () {
+        tmcat('MyModule -c ":"');
+        var actual = sh.cat('MyModule.ts');
+        var expected = sh.cat('correct_MyModule.ts');
+        assert.equal(actual, expected);
+    });
+
+    it('should translate error messages correctly', function () {
+        var actual = tmcat('MyModule -c "cat blah.tsc"');
+        var expected = sh.cat('blah.tmcat');
+        assert.equal(actual, expected);
+    });
+});
